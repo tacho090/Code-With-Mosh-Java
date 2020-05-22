2379b2de9ac211345cc989a3e4c5f22a68ba36f1
pablomora <pmora1990@gmail.com>
2020-05-22 17:28:14 -0600

    Add SetterInjection

---
+++ SetterInjection/src/com/helloWorld/Main.java
@@ -0,0 +1,13 @@
+package com.helloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var calculator = new TaxCalculator2020(100_000);
+        var report = new TaxReport(calculator);
+        report.show();
+
+        report.setCalculator(new TaxCalculator2021());
+        report.show();
+    }
+}
