1e074c90e433646a7f4db52018a100cf9e909ae3
pablomora <pmora1990@gmail.com>
2020-05-25 17:28:05 -0600

    Added Ultimate Java Series Part 3

---
+++ Ultimate-Java-Part-3/src/com/codewithmosh/Main.java
@@ -0,0 +1,9 @@
+package com.codewithmosh;
+
+import com.codewithmosh.exceptions.ExceptionsDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        ExceptionsDemo.show();
+    }
+}
